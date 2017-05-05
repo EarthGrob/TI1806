aa6e11b083858c4e16b2ff639dc3c0788bbb05cf
The Asian <earthtudelft@gmail.com>
2017-05-05 13:33:44 +0200

    Update Exercises.h

--- code/Exercises.h
+++ code/Exercises.h
@@ -7,7 +7,6 @@
 ////////////////// Exercise 1 ////////////////////////////////////
 std::pair<float, float> Statistics(const std::list<float>& values)
 {
-<<<<<<< HEAD
 	float sum = 0;
 	for (float i : values) {
 		sum += i;
@@ -24,26 +23,7 @@ std::pair<float, float> Statistics(const std::list<float>& values)
 
 
 	return std::pair<float, float>(mean, sd);
-=======
-	if (values.empty()) {
-		return std::pair<float, float>(0.f, 0.f);
-	}
-	int size = values.size();		//storing the size in a local variable for faster acces
-	float avg = 0.f;				//setting average and standerdDeviation to zero
-	float standerdDeviation = 0.f;  //so that we can add values to them.
 
-	for (float i: values) {			//foreach loop over the values of values
-		avg += i;					//adding everything to avg so it becomes the total sum
-	}
-	avg = avg/size;					//dividing avg by the number of values so that it becomes the average of the values
-
-	for (float i : values) {		//foreach over the values again
-		float temp = (i-avg);		//storing the current value minus the average
-		standerdDeviation += temp*temp; //adding the temporary value to standerdDeviation (it will become a sum of squares).
-	}
-	standerdDeviation = sqrt(standerdDeviation/size); //standerdDeviation will become the squareroot of standerdDeviation.
-	return std::pair<float, float>(avg, standerdDeviation); //returning the average first, then the standerdDeviation.
->>>>>>> d0de53c7bc4f6bf64142e290a98d9fea071b087b
 }
 //////////////////////////////////////////////////////////////////
 
