382bab57c01d34df01f2124a75ac4300e598f278
Earth Grob <earthtudelft@gmail.com>
2017-05-23 00:13:31 +0200

    Test

--- week3-submap/week3/yourcode.h
+++ week3-submap/week3/yourcode.h
@@ -171,7 +171,14 @@ Vec3Df blinnPhongSpecularOnly(const Vec3Df & vertexPos, Vec3Df & normal, const V
 //For v=Kd, return (c(N)+c(N+1))/2, else 0.
 Vec3Df toonShadingNoSpecular(const Vec3Df & vertexPos, Vec3Df & normal, const Vec3Df & lightPos, unsigned int index)
 {
-	return Vec3Df(0.5,0.5,0);
+	float f = Vec3Df::dotProduct(normal, (lightPos - vertexPos).unit());
+	// normalise
+	if (f < 0) f = 0;
+	// interval
+	float n = floorf(f * ToonDiscretize);
+	float interval = 1.0 / ToonDiscretize;
+	f = interval * n + (interval * 0.5f);
+	return Kd[index] * f;
 }
 
 //Toon shading specularity
@@ -179,7 +186,23 @@ Vec3Df toonShadingNoSpecular(const Vec3Df & vertexPos, Vec3Df & normal, const Ve
 //If a channel of Blinn-Phong Specularity has a value bigger or equal to ToonSpecularThreshold, then set it to 1, else to 0.
 Vec3Df toonShadingOnlySpecular(const Vec3Df & vertexPos, Vec3Df & normal, const Vec3Df & lightPos, const Vec3Df & cameraPos, unsigned int index)
 {
-	return Vec3Df(0,0,1);
+	//normalized light direction vector
+	Vec3Df nld = (lightPos - vertexPos).unit();
+	//make sure normal is unit vector just in case..
+	Vec3Df nn = normal.unit();
+	float nnDotNld = Vec3Df::dotProduct(nld, nn);
+	//check whether or not the light is on the right side of the surface.
+	if (nnDotNld < 0) {
+		return Vec3Df(0, 0, 0);
+	}
+	Vec3Df h = (cameraPos - (lightPos - vertexPos)).unit();
+	float dot = Vec3Df::dotProduct(h, normal.unit());
+
+	if (dot > ToonSpecularThreshold)
+		dot = 1;
+	else
+		dot = 0;
+	return Ks[index] * dot;
 }
 
 
@@ -191,7 +214,17 @@ Vec3Df userInteractionSphere(const Vec3Df & selectedPos, const Vec3Df & camPos)
 	//RETURN the new light position, defined as follows.
 	//selectedPos is a location on the mesh. Use this location to place the light source to cover the location as seen from camPos.
 	//Further, the light should be at a distance of 1.5 from the origin of the scene - in other words, located on a sphere of radius 1.5 around the origin.
-	return Vec3Df(1,1,1);
+	
+	float r = 1.5;
+	Vec3Df directionV = (selectedPos - camPos).unit();
+
+	float a = sqrt(pow(Vec3Df::dotProduct(directionV, camPos), 2) - camPos.getSquaredLength() + r*r);
+	float b = -(Vec3Df::dotProduct(directionV, camPos));
+	float d1 = b + a;
+	float d2 = b - a;
+
+	float distance = (d1 > d2) ? d2 : d1;
+	return (camPos + (directionV * distance));
 }
 
 Vec3Df userInteractionShadow(const Vec3Df & selectedPos, const Vec3Df & selectedNormal, const Vec3Df & lightPos)
@@ -200,7 +233,16 @@ Vec3Df userInteractionShadow(const Vec3Df & selectedPos, const Vec3Df & selected
 	//--- in this way, the shading boundary will be exactly at this location.
 	//there are several ways to do this, choose one you deem appropriate given the current light position
 	//no panic, I will not judge what solution you chose, as long as the above condition is met.
-	return Vec3Df(1,0,1);
+	// calculate difference
+	Vec3Df v = lightPos - selectedPos;
+	// calculate scalar of the normal
+	float scalar = Vec3Df::dotProduct(v, selectedNormal);
+	// subtract scalar times normal from the lightpos
+	// to obtain a point projected on the plane orthogonal
+	// to the scalar
+	Vec3Df proj = lightPos - scalar * selectedNormal;
+
+	return proj;	
 }
 
 Vec3Df userInteractionSpecular(const Vec3Df & selectedPos, const Vec3Df & selectedNormal, const Vec3Df & lightPos, const Vec3Df & cameraPos)
@@ -209,5 +251,9 @@ Vec3Df userInteractionSpecular(const Vec3Df & selectedPos, const Vec3Df & select
 	//please ensure also that the light is at a distance of 1 from selectedpos! If the camera is on the wrong side of the surface (normal pointing the other way),
 	//then just return the original light position.
 	//There is only ONE way of doing this!
-	return Vec3Df(0,1,1);
+	Vec3Df n = selectedNormal;
+	Vec3Df d = -1 * selectedPos;
+	Vec3Df r = d - 2 * Vec3Df::dotProduct(d, n) * n;
+	r.normalize();
+	return r;
 }
